880f6e66c623972a50184a9cf0c75b4110a3957f
Ayu Rahayu L <32546842+ayurahayul@users.noreply.github.com>
2017-12-01 04:33:50 +0700

    Update masaknasi.c

--- masaknasi.c
+++ masaknasi.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main () {
-	printf("langkah-langkah masak nasi \n");
-	printf("1.siapkan rice cocker \n");
+	printf("langkah-langkah memasak nasi \n");
+	printf("1.siapkan rice cooker \n");
 	printf("2.ambil beras \n");
 	printf("3.masukan beras ke dalam wadah \n");
 	printf("4.bersihkan beras \n");
 	printf("5.tuangkan air ke dalam wadah \n");
-	printf("6.masukan wadah ke rice cocker \n");
-	printf("7.tunggu sampai masak \n");
+	printf("6.masukan wadah ke rice cooker \n");
+	printf("7.nyalakan tombol cook \n");
+	printf("8.tunggu sampai masak \n");
+	printf("9.nasi siap dihidangkan \n");
 	return 0;
 }
